9e2d8831b9461061f72b54167b94fad60ed7db0e
Mariama-ny21 <mariama_ny21@yahoo.com>
2025-07-12 14:09:47 +0100

    Use dj_database_url for Heroku Postgres

--- meds_finder/settings.py
+++ meds_finder/settings.py
@@ -91,16 +91,10 @@ WSGI_APPLICATION = 'meds_finder.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
